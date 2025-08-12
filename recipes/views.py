c667cb16ed5b378d6ed7054927a7aa0584d34576
Nayra Rodrigues <nayra_iubriniz@outlook.com>
2025-08-11 23:15:49 -0300

    'criando novos arquivos'

--- recipes/views.py
+++ recipes/views.py
@@ -1,3 +1,19 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+
 
 # Create your views here.
+
+def home(response):
+    return HttpResponse('Home 2')
+    ...
+
+
+def contato(response):
+    return HttpResponse('Contato')
+    ...
+
+
+def sobre(response):
+    return HttpResponse('Sobre')
+    ...
